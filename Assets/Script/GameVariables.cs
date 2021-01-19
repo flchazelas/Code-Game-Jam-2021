cb0d8f2ac43c2a2f6c8dd4843c1c236c8b46a011
Hemji20 <77624213+Hemji20@users.noreply.github.com>
2021-01-19 23:50:58 +0100

    .

--- Assets/Script/GameVariables.cs
+++ Assets/Script/GameVariables.cs
@@ -7,4 +7,6 @@ public class GameVariables : MonoBehaviour
     public static int nbGold = 5;
     public static int nbArrow = 5;
     public static int nbHeart = 5;
+    public static int arrowDamage = 6;
+    public static double arrowLoading = 1.2;
 }
