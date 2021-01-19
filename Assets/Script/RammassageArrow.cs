93c9424f65e543ece9e03dfae933ef2382650041
Floran Chazelas <flo.chazelas@orange.fr>
2021-01-19 23:27:15 +0100

    Ajout rammassage fleche

---
+++ Assets/Script/RammassageArrow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RammassageArrow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision);
+        if (collision.transform.CompareTag("Player") && transform.parent.gameObject.GetComponent<ArrowBehaviour>().FinishThrow)
+        {
+            GameVariables.nbArrow++;
+            Destroy(transform.parent.gameObject);
+        }
+    }
+}
