cd637fba17288d9cfcf8f9666163a08bfdd44d62
Floran Chazelas <flo.chazelas@orange.fr>
2021-01-19 18:08:21 +0100

    GameVariable

---
+++ Assets/Script/GameVariable.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameVariable : MonoBehaviour
+{
+    public static int nbGold = 5;
+    public static int nbArrow = 5;
+
+    public static void buyItem(int price)
+    {
+        if(nbGold >= price)
+        {
+            for(int i=0; i<price; i++)
+            {
+                nbGold--;
+            }
+        }
+    }
+
+    public static void attackArrow()
+    {
+        nbArrow--;
+    }
+
+    public static void dropGold(string mob, int gain)
+    {
+        nbGold += gain;
+    }
+
+    public static void dropArrow(int gain)
+    {
+        nbArrow += gain;
+    }
+}
