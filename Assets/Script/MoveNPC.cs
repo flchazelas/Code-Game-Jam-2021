a3eb1da989a819ca7b57c93e61fe6abd5af5cb23
Siriex-Mathieu <60385180+Siriex-Mathieu@users.noreply.github.com>
2021-01-19 09:04:57 +0100

    Script Mob

---
+++ Assets/Script/MoveNPC.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveNPC : MonoBehaviour
+{
+    public float moveSpeed;
+
+    private Rigidbody2D myRigidbody;
+
+    public bool isWalking;
+
+    public float walkTime;
+    public float walkCounter;
+    public float waitTime;
+    public float waitCounter;
+
+    private int WalkDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>;
+
+        waitCounter = waitTime;
+        walkCounter = walkTime;
+
+        ChooseDirection();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isWalking)
+        {
+            walkCounter -= moveSpeed;
+            if(walkCounter < 0)
+            {
+                isWalking = false;
+                waitCounter = waitTime;
+            }
+        }
+        else
+        {
+
+        }
+    }
+
+    public void ChooseDirection()
+    {
+        WalkDirection = Random.Range(0, 4);
+        isWalking = true;
+        walkCounter = walkTime;
+    }
+}
