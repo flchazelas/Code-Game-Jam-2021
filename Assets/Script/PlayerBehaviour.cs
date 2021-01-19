46f2292ec646364de7279a4ca93b72530bc0a71e
Floran Chazelas <flo.chazelas@orange.fr>
2021-01-19 09:10:44 +0100

    Debug

---
+++ Assets/Script/PlayerBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    public float speed = 1f;
+
+    private Rigidbody2D rgb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rgb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Z))
+        {
+            
+        }
+    }
+}
