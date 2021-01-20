8d64404aca769bbb0722e9e555962ee7b577ae11
Hemji20 <77624213+Hemji20@users.noreply.github.com>
2021-01-20 01:31:03 +0100

    shop maj

--- Assets/Script/GameVariables.cs
+++ Assets/Script/GameVariables.cs
@@ -6,7 +6,7 @@ public class GameVariables : MonoBehaviour
 {
     public static int nbGold = 5;
     public static int nbArrow = 5;
-    public static int nbHeart = 5;
+    public static int nbHeart = 4;
     public static int arrowDamage = 6;
     public static float speedPrepare = 1f;
 }
--- Assets/Script/ShopSystem.cs
+++ Assets/Script/ShopSystem.cs
@@ -3,20 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ItemShopName : MonoBehaviour
+public class ShopSystem : MonoBehaviour
 {
     public GameObject item;
     public Text itemName;
     public Sprite itemView;
     public int price;
-    public new string name;
+    public bool trig;
+    public string name;
     
 
     // Start is called before the first frame update
     void Start()
     {
         itemView = GetComponent<SpriteRenderer>().sprite;
-        itemName.text = name + "\n" + price + "$";
+        itemName.text = name + " " + price.ToString() + "$";
         itemName.enabled = false;   
         
     }
@@ -24,34 +25,42 @@ public class ItemShopName : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.F) && trig)
+        {
+            if (name == "Level Up")
+            {
+                GameVariables.arrowDamage += 2;
+                GameVariables.speedPrepare += 0.5f;
+                InventoryFunction.buyItem(price);
+            }
+            else if (name == "Heart")
+            {
+                if (GameVariables.nbHeart < 5)
+                {
+                    Debug.Log("P");
+                    GameVariables.nbHeart++;
+                    InventoryFunction.buyItem(price);
+                }
+            }
+            else if (name == "Arrow")
+            {
+                GameVariables.nbArrow++;
+                InventoryFunction.buyItem(price);
+            }
+        }
     }
 
     void OnTriggerEnter2D(Collider2D col)
     {
+        
         if (col.tag == "Player")
         {
+            trig = true;
+            Debug.Log(itemName.text);
             itemName.enabled = true;
         }
 
-        if (Input.GetKey(KeyCode.F))
-        {
-            InventoryFunction.buyItem(price);
 
-            if(name == "Arc" || name == "arc")
-            {
-                GameVariables.arrowDamage += 2;
-                GameVariables.speedPrepare += 0.5f;
-            }
-            else if(name == "Heart" || name == "heart")
-            {
-                GameVariables.nbHeart++;
-            }
-            else if(name == "Arrow" || name == "arrow")
-            {
-                GameVariables.nbArrow++;
-            }
-        }
     }
 
     void OnTriggerExit2D(Collider2D col)
