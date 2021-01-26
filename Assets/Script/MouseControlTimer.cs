0df3a87a059762c33224cf664bc8323cd9df9ae8
Ciberdead <38950115+Ciberdead@users.noreply.github.com>
2021-01-26 19:20:49 +0100

    nico modif perso

---
+++ Assets/Script/MouseControlTimer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MouseControlTimer : MonoBehaviour
+{
+
+    public bool showUI = false;
+    public GameObject uiParent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        showUI = false;
+        Invoke("leftMB", 1.0f);
+    }
+
+    void leftMB()
+    {
+        showUI = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
