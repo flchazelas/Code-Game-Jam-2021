afd0aee9e93a8218c5d799420a4ab2e112808782
Ciberdead <38950115+Ciberdead@users.noreply.github.com>
2021-01-29 10:25:22 +0100

    ajout librairie

---
+++ Assets/Script/EnemyBeviahour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
+
+public class EnemyBeviahour : MonoBehaviour
+{
+
+    private EnemyPathfindingMovement pathfindingMovement;
+    public float moveSpeedBase;
+    public float moveSpeedCmbt;
+
+
+    private Vector3 startingPosition;
+    
+    
+    private void Start()
+    {
+        startingPosition = transform.position;
+    }
+
+    private Vector3 GetRoamingPosition()
+    {
+        Vector3 move;   
+        move = UtilsClass.GetRandomDir() * Random.Range(10f,50f);
+        return move;
+    }
+}
